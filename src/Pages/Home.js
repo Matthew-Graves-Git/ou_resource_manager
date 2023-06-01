b7ff8c8b66a2eb5400662956ce47c6b1f7200524
Matthew Graves <matthew.graves.cs@gmail.com>
2023-06-01 11:33:18 -0400

    quailty changes to pages

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../Components/css/style.css';
 import DisplayCard from '../Components/DisplayCard';
 import ItemDescriptionCard from '../Components/ItemDescriptionCard';
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -8,6 +8,7 @@ const Login = () => {
     const [username, setusername] = useState("");
     const [password, setpassword] = useState("");
     const [loggedIn, setloggedIn] = useState(false);
+    const [error, seterror] = useState(null);
     const Auth = useContext(AuthContext);
     const navigate = useNavigate();
     const handleSubmit = async (e) => {
@@ -20,32 +21,36 @@ const Login = () => {
             const user = { id, name, role, authdata };
             Auth.userLogin(user);
             setloggedIn(true);
+            document.body.style.background = "#e9e9e9"
             navigate("/Home")
           })
-          .catch(error => {
+          .catch((e) => {
+            seterror(e);
             console.log(error);
           })
       }
-
+    const revealErr = () =>{
+      return <p>Bad Credentials, Try Again</p>
+    }
     return ( 
-      
-      <form onSubmit={handleSubmit}>
-        <div className="container">
-        <label>
-          UserName
-          <textarea value={username} onChange={(e) => {setusername(e.target.value)}} />
-        </label>
-        <label>
-          Password
-          <textarea value={password} onChange={(e) => {setpassword(e.target.value)}} />
-        </label>
-        <button type="submit">submit</button>
-        </div>
-      </form>
-        // <>
-        // <button onClick={handleSubmit}>Login</button>
-        // </>
+      <>
         
+        <div className="login-box">
+        <h2>Login</h2>
+        {error && revealErr()}
+        <form onSubmit={handleSubmit}>
+          <div className="user-box">
+            <input type="text" value={username} onChange={(e) => {setusername(e.target.value)}}/>
+            <label>Username</label>
+          </div>
+          <div className="user-box">
+            <input type="password" value={password} onChange={(e) => {setpassword(e.target.value)}}/>
+            <label>Password</label>
+          </div>
+          <button type="submit" className="button">Sign In</button>
+        </form>
+      </div>
+      </>
       
     );
 }
