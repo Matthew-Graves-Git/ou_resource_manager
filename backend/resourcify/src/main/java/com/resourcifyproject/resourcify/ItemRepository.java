c434561c099228aa043064945dab1fb0770085f5
James-D-Oakland <134322104+James-D-Oakland@users.noreply.github.com>
2023-06-18 17:40:01 -0400

    small update

--- backend/resourcify/src/main/java/com/resourcifyproject/resourcify/ItemRepository.java
+++ backend/resourcify/src/main/java/com/resourcifyproject/resourcify/ItemRepository.java
@@ -1,12 +1,13 @@
 package com.resourcifyproject.resourcify;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-    //List<Item> findByUsernameAndItemtype(String username, ItemType itemType);
-//    Optional<Item> findById(Integer item_Id);
-    //int countItems(Resource resource, ItemType itemType, boolean isAvailable);
-    //Need to make this work as JPA custom query
+    //List<Item> findByUsernameAndItemtype(String username, ItemType itemType);//
+    @Query(value = "SELECT COUNT(*) FROM ITEM WHERE RESOURCE = ?1 AND ITEMTYPE = ?2 AND ISAVAILABLE = ?3", nativeQuery = true)
+    int countItems(Resource resource, ItemType itemType, boolean isAvailable);
 }
