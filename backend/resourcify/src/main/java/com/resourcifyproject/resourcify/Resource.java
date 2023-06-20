eb1444e8a323d13d69f4aeaabde86c3ca9f7da26
JamesDimancea <98126129+JamesDimancea@users.noreply.github.com>
2023-06-20 19:11:17 -0400

    fixed small problem

--- backend/resourcify/src/main/java/com/resourcifyproject/resourcify/Resource.java
+++ backend/resourcify/src/main/java/com/resourcifyproject/resourcify/Resource.java
@@ -14,32 +14,37 @@ public class Resource {
     @Column(name="resource_category")
     private ResourceCategory resourceCategory;
 
-    private int quantityBorrow = 0, quantitySale = 0;
     private String name, description, image, modelNumber;
     private float salePrice, borrowPrice;
 
 
-    //no setter for ID
+    public Integer getResourceId() { return resourceId; } //no setter for ID
+
+    public ResourceCategory getResourceCategory() { return resourceCategory; }
 
     public void setResourceCategory(String resourceCategory) {
         this.resourceCategory = ResourceCategory.valueOf(resourceCategory);
     }
 
-    public void setQuantityBorrow(int quantityBorrow) { this.quantityBorrow = quantityBorrow; }
-
-    public void setQuantitySale(int quantitySale) { this.quantitySale = quantitySale; }
+    public String getName() { return name; }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getImage() { return image; }
+
     public void setImage(String image) {
         this.image = image;
     }
 
+    public String getModelNumber() { return modelNumber; }
+
     public void setModelNumber(String model) {
         this.modelNumber = model;
     }
+
+    public String getDescription() { return description; }
 
     public void setDescription(String description) {
         this.description = description;
