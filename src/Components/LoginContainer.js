f6849ce71db3fc5b36a142a5b1ac260953944651
JamesDimancea <jamesdimancea@oakland.edu>
2023-06-22 06:26:40 -0400

    Updated with Nadia's UI
    I never went to sleep. Figuring out how to get to here was maddening.
    Login footer is broken for some reason.

--- src/Components/LoginContainer.js
+++ src/Components/LoginContainer.js
@@ -1,7 +1,13 @@
 import {Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const LoginContainer = () => {
-    return ( <Outlet/> );
+    return (<>
+        <Navbar isLoginPage="true" />
+        <Outlet/>
+        <Footer/> 
+    </>);
 }
  
 export default LoginContainer;
